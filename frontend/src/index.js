c2915ffe40c07718f404d083c09e264c137c8001
Ankush Goel <goelankush1312@gmail.com>
2024-06-24 18:33:46 +0530

    added store to App using Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
-import App from './App';
-import HomePage from './pages/HomePage';
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
+import App from './App'
+import HomePage from './pages/HomePage'
 
-import reportWebVitals from './reportWebVitals';
-import ProductPage from './pages/ProductPage';
+import reportWebVitals from './reportWebVitals'
+import ProductPage from './pages/ProductPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,16 +19,18 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductPage />} />
     </Route>
   )
-);
+)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
