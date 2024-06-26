2640163ce45e5939a710bd7ee0c90c73811c409f
Ankush Goel <goelankush1312@gmail.com>
2024-06-26 22:32:55 +0530

    Fetching products using products api slice(RTK Query)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,32 +1,30 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, isError, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const getProducts = async () => {
-      const {data} = await axios.get('/api/products');
-      // console.log(data);
-      setProducts(data);
-    }
-    getProducts()
-  }, [])
-  
   return (
     <>
-    <h1>Latest Products</h1>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : isError ? (
+        <div>{error?.data?.message || error?.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
           <Row>
             {products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                 <Product product={product} />
               </Col>
             ))}
-          </Row></>
-  )
-}
+          </Row>
+        </>
+      )}
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
