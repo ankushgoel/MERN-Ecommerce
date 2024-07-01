30fac084b859ad2312c85d263597a349ec299e98
Ankush Goel <goelankush1312@gmail.com>
2024-07-01 16:51:48 +0530

    cartSlice & addToCart reducer/action

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,6 +2,10 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: [] };
 
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
@@ -9,8 +13,21 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = action.payload;
       state.cartItems = [...state.cartItems, item];
+
+      // Calculate cart item price
+      state.itemsPrice = state.cartItems.reduce((acc, item) => acc + item.price * item, 0);
+
+      // Calculate the shipping price | If items price is greater than 499, then shipping is free | If not, shipping is 50
+      state.shippingPrice = addDecimals(state.itemsPrice > 499 ? 0 : 50);
+
+      // Calculate total price
+      state.totalPrice = (Number(state.itemsPrice) + Number(state.shippingPrice)).toFixed(2);
+
+      localStorage.setItem('cart', JSON.stringify(state));
     },
   },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
