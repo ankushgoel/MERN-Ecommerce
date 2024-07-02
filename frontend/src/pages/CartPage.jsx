a319537e01a9a93f1a1143736e48e9575073621b
Ankush Goel <goelankush1312@gmail.com>
2024-07-02 23:40:37 +0530

    Shopping cart items

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Row, Col, Card, Image, ListGroup, Button, Alert, Form } from 'react-bootstrap';
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft, FaTrash } from 'react-icons/fa';
 
 const CartPage = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -21,11 +21,57 @@ const CartPage = () => {
             <ListGroup variant="flush">
               {cartItems.map((item) => (
                 <ListGroup.Item key={item._id}>
-                  <span>{item.name}</span>
+                  <Row>
+                    <Col md={3}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col className="row">
+                      <Row>
+                        <Link to={`/product/${item._id}`}>{item.name}</Link>
+                      </Row>
+                      {/* Quantity & Delete */}
+                      <Row className="align-self-end">
+                        <Col md={3} lg={2}>
+                          <Form.Control as="select" value={item.qty} onChange={(e) => {}}>
+                            {[...Array(item.countInStock).keys()].map((x) => (
+                              <option value={x + 1} key={x + 1}>
+                                {x + 1}
+                              </option>
+                            ))}
+                          </Form.Control>
+                        </Col>
+                        <Col md={2}>
+                          <Button type="button" variant="light">
+                            <FaTrash />
+                          </Button>
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col md={1}>
+                      <span>₹{item.price}</span>
+                    </Col>
+                  </Row>
                 </ListGroup.Item>
               ))}
             </ListGroup>
           )}
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h4>
+                  Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)} items): ₹
+                  {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
+                </h4>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button type="button" className="btn-block" disabled={cartItems.length === 0}>
+                  Proceed to Checkout
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </>
