f0c269f4dd6de44920a22ed17df3855bb545a9de
Ankush Goel <goelankush1312@gmail.com>
2024-07-04 17:36:43 +0530

    Added auth middleware and protected routes

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from './asyncHandler.js';
+import User from '../models/userModel.js';
+
+// Protected routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token = req.cookies.jwt;
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select('-password');
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('not authorized, token mismatch!');
+    }
+  } else {
+    res.status(401);
+    throw new Error('not authorized, no token!');
+  }
+});
+
+// Admin Middleware
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('not authorized!');
+  }
+};
+
+export { protect, admin };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,13 +10,18 @@ import {
   updateUser,
   deleteUser,
 } from '../controllers/userController.js';
+import { admin, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.route('/profile').put(updateUserProfile).get(getUserProfile);
-router.route('/:id').put(updateUser).get(getUserById).delete(deleteUser);
+router.route('/profile').put(protect, updateUserProfile).get(protect, getUserProfile);
+router
+  .route('/:id')
+  .put(protect, admin, updateUser)
+  .get(protect, admin, getUserById)
+  .delete(protect, admin, deleteUser);
 
 export default router;
