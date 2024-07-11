a2e73d0a940ecf4c0a39043fd76633fcdb9d7c85
Ankush Goel <goelankush1312@gmail.com>
2024-07-11 19:22:35 +0530

    some minor fixes

--- frontend/src/components/CheckoutSteps.jsx
+++ frontend/src/components/CheckoutSteps.jsx
@@ -5,37 +5,22 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
     <Nav className="mb-4 justify-content-center align-items-center">
       <Nav.Item>
-        <Nav.Link
-          className="checkout-step"
-          disabled={`${step1 ? '' : 'true'}`}
-          as={Link}
-          to="/cart"
-        >
+        <Nav.Link className="checkout-step" disabled={`${step1 ? '' : 1}`} as={Link} to="/cart">
           Cart
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link
-          className="checkout-step"
-          disabled={`${step2 ? '' : 'true'}`}
-          as={Link}
-          to="/shipping"
-        >
+        <Nav.Link className="checkout-step" disabled={`${step2 ? '' : 1}`} as={Link} to="/shipping">
           Shipping
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link
-          className="checkout-step"
-          disabled={`${step3 ? '' : 'true'}`}
-          as={Link}
-          to="/payment"
-        >
+        <Nav.Link className="checkout-step" disabled={`${step3 ? '' : 1}`} as={Link} to="/payment">
           Payment
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link disabled={`${step4 ? '' : 'true'}`} as={Link} to="/placeorder">
+        <Nav.Link disabled={`${step4 ? '' : 1}`} as={Link} to="/placeorder">
           Place Order
         </Nav.Link>
       </Nav.Item>
--- frontend/src/pages/ShippingPage.jsx
+++ frontend/src/pages/ShippingPage.jsx
@@ -9,11 +9,11 @@ const ShippingPage = () => {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
 
-  const [address, setAddress] = useState(shippingAddress.address || '');
-  const [city, setCity] = useState(shippingAddress.city || '');
-  const [state, setState] = useState(shippingAddress.state || '');
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || '');
-  const [country, setCountry] = useState(shippingAddress.country || '');
+  const [address, setAddress] = useState(shippingAddress?.address || '');
+  const [city, setCity] = useState(shippingAddress?.city || '');
+  const [state, setState] = useState(shippingAddress?.state || '');
+  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode || '');
+  const [country, setCountry] = useState(shippingAddress?.country || '');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
