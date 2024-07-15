518ecca08d84fb7d40c456c8e583a0d6833d2913
Ankush Goel <goelankush1312@gmail.com>
2024-07-15 20:44:01 +0530

    Added fetch order details by Id in api slice

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -49,7 +49,7 @@ const getMyOrders = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user', 'name email');
+  const order = await Order.findById(req.params.id).populate('user', 'firstName lastName email');
   if (order) {
     res.json(order);
   } else {
